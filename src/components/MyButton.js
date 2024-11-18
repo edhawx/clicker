97cb28bac236ab5bdc27b0cb1b3c14d5d95409da
edhawx <deadlifecorps@gmail.com>
2024-11-18 22:25:57 +0000

    Preload audio files in myButton

--- src/components/MyButton.js
+++ src/components/MyButton.js
@@ -4,19 +4,32 @@ import "../styles/MyButton.css";
 function MyButton() {
   const [count, setCount] = useState(0);
   const [isGreen, setIsGreen] = useState(false); 
+  const [clickSound, setClickSound] = useState(null);
+  const [snapSound, setSnapSound] = useState(null);
 
-  const clickSound = new Audio(require("../assets/sounds/click.wav"));
-  const snapSound = new Audio(require("../assets/sounds/snap.wav"));
+
+  useEffect(()=>{
+    const clickSound = new Audio(require("../assets/sounds/click.wav"));
+    const snapSound = new Audio(require("../assets/sounds/snap.wav"));
+    setClickSound(clickAudio);
+    setSnapSound(snapAudio);
+
+    return()=>{
+        clickAudio.src ="";
+        snapAudio.src ="";
+    };
+  }, []);
+
 
   const handleClick = () => {
     if (count === 9) {
-      snapSound.play();
+      if(snapSound) snapSound.play();
       setIsGreen(true); 
       setTimeout(() => {
         setIsGreen(false); 
       }, 1000);
     } else {
-      clickSound.play();
+      if (clickSound) clickSound.play();
     }
 
     setCount((prevCount) => (prevCount + 1) % 10);
